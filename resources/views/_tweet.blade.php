73d19688a928da8f06d8926ea03ea5104eb2e66e
Putro Budi <putromulyobudi@gmail.com>
2021-03-17 13:04:41 +0700

    Design the Timeline

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,12 @@
+<div class="flex p-4 border-b border-b-gray-400">
+  {{-- You can do <aside> or just simple <div> --}}
+  <div class="mr-2 flex-shrink-0"> {{-- flex-shrink so it's not affected by flex p-4 above so you can resize it. --}}
+      <img src="https://i.pravatar.cc/50" alt="" class="rounded-full mr-2">
+  </div>
+  <div>
+      <h5 class="font-bold mb-4">John Doe</h5>
+      <p class="text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur unde eaque natus aliquid cum nemo sequi repellendus, rerum dicta repudiandae! Similique quo minus sunt recusandae?
+      </p>
+  </div>
+</div>
