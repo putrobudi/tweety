01caa5f37911ffb805ce97c6d76c4d19c582c965
Putro Budi <putromulyobudi@gmail.com>
2021-03-20 16:27:51 +0700

    Nested Layout Files Using Components

---
+++ resources/views/components/app.blade.php
@@ -0,0 +1,25 @@
+{{-- refence blade component --}}
+{{-- @component('master') --}}
+<x-master> {{-- Laravel 7 and above x-componentName --}}
+    {{-- anything within this tag will be slotted in {{ $slot }}, the master component. --}}
+    <section class="px-8">
+        <main class="container mx-auto">
+            <div class="lg:flex lg:justify-between">
+                @if (auth()->check()) {{-- using this method, it throws a feeling that it'd break 
+                    the moment you'd add more flecibility. So let's use blade components. --}}
+                    <div class="lg:w-32">
+                        @include('_sidebar-links')
+                    </div>
+                @endif
+                <div class="lg:flex-1 lg:mx-10" style="max-width: 700px">
+                    {{ $slot }}
+                </div>
+                @if (auth()->check())
+                    <div class="lg:w-1/6 bg-blue-100 rounded-lg p-4">
+                        @include('_friends-list')
+                    </div>
+                @endif
+            </div>
+        </main>
+    </section>
+</x-master>
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-auth()->loginUsingId(1);
+// auth()->loginUsingId(1);
 Route::get('/', function () {
     return view('welcome');
 });
